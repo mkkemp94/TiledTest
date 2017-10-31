c6a10b09fc1dad7a601fe319ecda9e8bba13c585
Michael Kemp <mkemp@accu-time.com>
2017-10-31 15:27:43 -0400

    Sprites Move in the correct layer

--- desktop/src/com/mygdx/tiledtest/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/tiledtest/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.mygdx.tiledtest.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.tiledtest.TiledTest;
+import com.mygdx.tiledtest.TiledTestWithSpriteRenderer;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new TiledTest(), config);
+		new LwjglApplication(new TiledTestWithSpriteRenderer(), config);
 	}
 }
